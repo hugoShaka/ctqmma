9cc25c786801fafc45df8bd24c1e511b5eeee353
Mathieu <fakeadr@free.fr>
2018-03-01 19:26:33 +0100

    Adding Trajet class

---
+++ trajets.py
@@ -0,0 +1,22 @@
+
+class Trajet(object):
+    
+    def __init__(self,ident,a,b,x,y,s,f):
+        self.ident=ident
+        self.a=a
+        self.b=b
+        self.x=x
+        self.y=y
+        self.s=s
+        self.f=f
+        self.estReserve=False
+        self.distance=abs(a-x)+abs(b-y)
+        
+    def printTraj(self):
+        print("(%d:%d) -> (%d:%d), start = %d, finish = %d, estReserve = %r, distance = %d" % (self.a, self.b, self.x, self.y, self.s, self.f, self.estReserve, self.distance))
+        
+def exampleTraj():
+    traj1=Trajet(1,2,3,4,5,6,7);
+    traj1.printTraj()
+        
+#example()
