63d3f273fda26add0f301eb498dc23ea0a36a7e8
Mathieu <fakeadr@free.fr>
2018-03-01 19:32:59 +0100

    Adding distance functions

--- trajets.py
+++ trajets.py
@@ -15,8 +15,16 @@ class Trajet(object):
     def printTraj(self):
         print("(%d:%d) -> (%d:%d), start = %d, finish = %d, estReserve = %r, distance = %d" % (self.a, self.b, self.x, self.y, self.s, self.f, self.estReserve, self.distance))
         
+    def getStartDistFrom(self,ptX,ptY):
+        return (abs(self.a-ptX)+abs(self.b-ptY))
+    
+    def getEndDistFrom(self,ptX,ptY):
+        return (abs(self.x-ptX)+abs(self.y-ptY))
+        
 def exampleTraj():
     traj1=Trajet(1,2,3,4,5,6,7);
     traj1.printTraj()
+    print (str(traj1.getStartDistFrom(0,0)))
+    print (str(traj1.getEndDistFrom(0,0)))
         
-#example()
+#exampleTraj()
